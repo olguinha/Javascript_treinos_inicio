9bd6c75a643d20022f6d40533e4df9fd2e166afc
Olga Oliveira <105463799+olguinha@users.noreply.github.com>
2025-12-07 20:07:06 -0400

    como pegar elementos pai e filho no js
    desenvolvimento js

---
+++ Elemento DOM/Aula13/script.js
@@ -0,0 +1,22 @@
+"use scrict";
+
+const strong = document.querySelector("strong");
+const p  = document.querySelector("p");
+const container  = document.querySelector("container");
+
+
+// estes elementos retornam os elementos pais do elemento selecionado. 
+// O pai sempre será a camada de cima do seu elemento HTML.
+
+console.log(p.parentElement);
+console.log(p.parentNode);
+
+console.log(document.documentElement.parentNode === document);
+console.log(document.documentElement.parentNode === document);
+
+
+
+
+
+
+
---
+++ Elemento DOM/Aula14/script.js
@@ -0,0 +1,16 @@
+"use scrict";
+
+
+
+const container = document.querySelector(".container");
+console.log(container.children);
+console.log(container.childNodes);
+
+
+const children = Array.from(container.children);
+console.log(children);
+
+children.forEach((res) => {
+    console.log(res);
+})
+
